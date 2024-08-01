5432798e042f879ecbe257bdcaadbd74a0e843a9
infanasotku <infanasotku@gmail.com>
2024-08-01 21:25:27 +0700

    Added simple task

--- src/xray.py
+++ src/xray.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator, Coroutine
 from fastapi import FastAPI
-from fastapi_utils.tasks import repeat_every
+from fastapi_utils.tasks import repeat_every, NoArgsNoReturnAsyncFuncT
 import subprocess as sub
 import settings
 
@@ -15,14 +15,20 @@ def create() -> FastAPI:
 class Xray:
     def __init__(self):
         self._executable_dir = settings.get().xray_executable_dir
-        self._inst = sub.Popen()
+        # self._inst = sub.Popen()
 
     async def lifespan(self, _: FastAPI) -> AsyncGenerator:
-        restart_task = asyncio.create_task(self.restart())
+        restart_task = asyncio.create_task(self._run_restart_task())
         yield
         restart_task.cancel()
         yield
 
-    @repeat_every(seconds=5, logger=logging.getLogger("uvicorn.error"))
-    def restart(self):
+    def _run_restart_task(self) -> Coroutine[Any, Any, None]:
+        @repeat_every(seconds=5, logger=logging.getLogger("uvicorn.error"))
+        def restart():
+            self._restart()
+
+        return restart()
+
+    def _restart(self):
         print(f"hello! {self._executable_dir}")
