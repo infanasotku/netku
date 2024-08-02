d3330467a0cbd8c14a5345bec1d46708bce0f2c6
infanasotku <infanasotku@gmail.com>
2024-08-02 14:32:32 +0700

    Deleted extra

--- src/settings.py
+++ src/settings.py
@@ -11,15 +11,15 @@ from dotenv import load_dotenv
 
 
 class Settings(BaseSettings):
-    #region Network
+    # region Network
     host: str = Field(validation_alias="HOST", default="127.0.0.1")
     port: int = Field(validation_alias="PORT", default=5100)
     domain: str = Field(validation_alias="DOMAIN", default="127.0.0.1")
     ssl_keyfile: Optional[str] = Field(validation_alias="SSL_KEYFILE", default=None)
     ssl_certfile: Optional[str] = Field(validation_alias="SSL_CERTFILE", default=None)
-    #endregion
+    # endregion
 
-    #region Xray
+    # region Xray
     xray_executable_dir: str = Field(validation_alias="XRAY_EXECUTABLE_DIR")
     xray_executable_name: str = Field(validation_alias="XRAY_EXECUTABLE_NAME")
     xray_restart_minutes: float = Field(validation_alias="XRAY_RESTART_MINUTES")
@@ -29,15 +29,16 @@ class Settings(BaseSettings):
     @property
     def xray_config(self) -> dict:
         try:
-            logging.info(f"Loading xray config")
+            logging.info("Loading xray config")
             with open(f"{self.app_directory_path}/xray_config.json", "r") as f:
                 config = json.loads(f.read())
-                logging.info(f"Xray config loaded")
+                logging.info("Xray config loaded")
                 return config
         except Exception as e:
             logging.critical(f"Xray config loaded with error: {e}")
             sys.exit(1)
-    #endregion
+
+    # endregion
 
     @computed_field
     @property
