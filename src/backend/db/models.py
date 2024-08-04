7f65097943e15ea8a5c541ca2d2baff864cc1cf6
infanasotku <infanasotku@gmail.com>
2024-08-04 14:36:10 +0700

    Started work with bd

---
+++ src/backend/db/models.py
@@ -0,0 +1,14 @@
+from db.database import Base
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import mapped_column, Mapped
+from typing import Annotated
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+
+class User(Base):
+    __tablename__ = "workers"
+
+    id: Mapped[intpk]
+    phone_number: Mapped[str] = mapped_column(nullable=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=True)
---
+++ src/backend/db/schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class BaseSchema(BaseModel):
+    class Config:
+        from_attributes = True
+
+    id: int
