a18ec6f9ee8f3fd830106049722fdeb42531c793
infanasotku <infanasotku@gmail.com>
2024-08-04 15:27:57 +0700

    Added registration

--- src/backend/bot/router.py
+++ src/backend/bot/router.py
@@ -1,15 +1,54 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
+from bot.states import BaseState
+import bot.utils as utils
+
 
 router = Router(name="main")
 
 
-@router.message(Command("start"))
-async def start(message: Message):
-    await message.answer("Subscription completed!")
+@router.message(
+    Command("start"),
+)
+async def start(message: Message, state: FSMContext):
+    user = utils.get_user(message)
+
+    if user:
+        await state.set_state(BaseState.none)
+        await message.answer(f"Hello! {message.from_user.first_name}!")
+    else:
+        await state.set_state(BaseState.registration)
+        share_phone = KeyboardButton(text="Поделиться", request_contact=True)
+        keyboard = ReplyKeyboardMarkup(keyboard=[[share_phone]])
+        await message.answer(
+            "Hello! For identification you need to share your phone.",
+            reply_markup=keyboard,
+        )
 
 
 @router.message(Command("stop"))
 async def stop(message: Message):
     await message.answer("Subscription canceled!")
+
+
+@router.message(BaseState.registration)
+async def registrate(message: Message):
+    if not message.contact:
+        await utils.try_delete_message(message)
+        return
+
+    user = utils.registrate_user(message.contact)
+    if user:
+        await message.answer("Subscription registrated!")
+    if not user:
+        await message.answer(
+            "Sorry, you have not enough permissions for subscription",
+        )
+
+
+@router.message(BaseState.none)
+async def delete_extra(message: Message):
+    await utils.try_delete_message(message)
