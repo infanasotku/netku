7da4e185601c0a5833b84dfb584ba3b756d8745e
infanasotku <infanasotku@gmail.com>
2024-09-07 13:25:26 +0700

    Fixed formatting

--- services/assistant/xray/handler/handler_pb2.py
+++ services/assistant/xray/handler/handler_pb2.py
@@ -4,37 +4,36 @@
 # source: xray/handler/handler
 # Protobuf Python Version: 5.27.2
 """Generated protocol buffer code."""
+
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC,
-    5,
-    27,
-    2,
-    '',
-    'xray/handler/handler'
+    _runtime_version.Domain.PUBLIC, 5, 27, 2, "", "xray/handler/handler"
 )
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-
-
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14xray/handler/handler\x12\x07handler\"\x1f\n\x0fRestartResponse\x12\x0c\n\x04uuid\x18\x01 \x01(\t\"\x06\n\x04Null2J\n\x0eHandlerService\x12\x38\n\x0bRestartXray\x12\r.handler.Null\x1a\x18.handler.RestartResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n\x14xray/handler/handler\x12\x07handler"\x1f\n\x0fRestartResponse\x12\x0c\n\x04uuid\x18\x01 \x01(\t"\x06\n\x04Null2J\n\x0eHandlerService\x12\x38\n\x0bRestartXray\x12\r.handler.Null\x1a\x18.handler.RestartResponse"\x00\x62\x06proto3'
+)
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'xray.handler.handler_pb2', _globals)
+_builder.BuildTopDescriptorsAndMessages(
+    DESCRIPTOR, "xray.handler.handler_pb2", _globals
+)
 if not _descriptor._USE_C_DESCRIPTORS:
-  DESCRIPTOR._loaded_options = None
-  _globals['_RESTARTRESPONSE']._serialized_start=33
-  _globals['_RESTARTRESPONSE']._serialized_end=64
-  _globals['_NULL']._serialized_start=66
-  _globals['_NULL']._serialized_end=72
-  _globals['_HANDLERSERVICE']._serialized_start=74
-  _globals['_HANDLERSERVICE']._serialized_end=148
+    DESCRIPTOR._loaded_options = None
+    _globals["_RESTARTRESPONSE"]._serialized_start = 33
+    _globals["_RESTARTRESPONSE"]._serialized_end = 64
+    _globals["_NULL"]._serialized_start = 66
+    _globals["_NULL"]._serialized_end = 72
+    _globals["_HANDLERSERVICE"]._serialized_start = 74
+    _globals["_HANDLERSERVICE"]._serialized_end = 148
 # @@protoc_insertion_point(module_scope)
