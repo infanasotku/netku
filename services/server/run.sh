93a61bb9b70675d75b6f964eb7fbd48faa3e040a
infanasotku <infanasotku@gmail.com>
2024-09-07 12:23:14 +0700

    Changed run script for server

--- services/server/run.sh
+++ services/server/run.sh
@@ -1,4 +1,8 @@
-{ echo & \
- envsubst '${DOMAIN},${PORT}' \
- < /etc/nginx/conf.templates/default.conf; } \
- >> /etc/nginx/conf.d/infanasotku.conf
+touch temp
+envsubst '${DOMAIN},${PORT}' \
+ < /etc/nginx/conf.templates/default.conf \
+ > temp
+echo >> temp
+cat /etc/nginx/conf.d/infanasotku.conf >> temp
+cat temp > /etc/nginx/conf.d/infanasotku.conf
+rm temp
