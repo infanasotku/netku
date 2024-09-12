97b40aa01929b79095fcd92d95d33cccd7f87a55
infanasotku <infanasotku@gmail.com>
2024-09-12 09:20:34 +0700

    Completed nav bar

--- services/landing/src/components/NavBar.vue
+++ services/landing/src/components/NavBar.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavGroup from "@/components/NavGroup.vue";
+
+const props = defineProps({
+  navGroups: {
+    type: Array<{
+      header: string;
+      list: Array<{ active: boolean; content: string; href: string }>;
+    }>,
+    required: true,
+  },
+});
+</script>
 
 <template>
-  <nav class="navbar"></nav>
+  <nav class="navbar">
+    <NavGroup
+      class="nav-group"
+      v-for="group in props.navGroups"
+      :header="group.header"
+      :list="group.list"
+    ></NavGroup>
+  </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  gap: 15px;
+}
+</style>
--- services/landing/src/components/NavGroup.vue
+++ services/landing/src/components/NavGroup.vue
@@ -1,7 +1,60 @@
-<template lang="">
-  <div></div>
-</template>
-<script>
-export default {};
+<script setup lang="ts">
+const props = defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+  list: {
+    type: Array<{ active: boolean; content: string; href: string }>,
+    required: true,
+  },
+});
 </script>
-<style lang=""></style>
+<template>
+  <ul class="group">
+    <li class="group-header">{{ props.header }}</li>
+    <li v-for="link in props.list">
+      <a :class="{ highlighted: link.active }" :href="link.href">
+        {{ link.content }}</a
+      >
+    </li>
+  </ul>
+</template>
+<style scoped>
+.group {
+  list-style: none;
+  padding-left: 0;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  margin: 0;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  border-top: 1px solid var(--divider-color);
+}
+.group-header {
+  color: var(--text-color-1) !important;
+  font-size: 14px;
+  font-weight: 600;
+}
+.group li {
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+.group li a {
+  color: var(--text-color-2);
+  font-size: 14px;
+  font-weight: 600;
+  transition: 0.5s;
+}
+.group li:not(:first-child):hover a,
+.group li:not(:first-child):hover {
+  cursor: pointer;
+  color: var(--highlight-color-1);
+}
+.highlighted {
+  color: var(--highlight-color-1) !important;
+}
+</style>
