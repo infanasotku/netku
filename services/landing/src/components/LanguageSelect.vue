c91dcb94a542b991b4655580c9476b53e47e374b
infanasotku <infanasotku@gmail.com>
2024-09-16 20:05:51 +0700

    Added dynamic menu list/Added hover styles

--- services/landing/src/components/LanguageSelect.vue
+++ services/landing/src/components/LanguageSelect.vue
@@ -3,88 +3,92 @@ import SelectMenu from "@/components/SelectMenu.vue";
 import { ref } from "vue";
 
 const menuVisible = ref(false);
+
+const menu = ref(["English", "Русский"]);
+const current = ref(0);
 </script>
 
 <template>
-  <div
-    class="wrapper"
-    @mouseenter="menuVisible = true"
-    @mouseleave="menuVisible = false"
-  >
-    <button class="select">
-      <span class="icon">
-        <span class="language select-icon"></span>
-        <span class="arrow select-icon"></span>
-      </span>
-    </button>
-    <Transition name="menu">
-      <div class="menu" v-if="menuVisible">
-        <SelectMenu></SelectMenu>
-      </div>
-    </Transition>
-  </div>
+	<div
+		class="wrapper"
+		@mouseenter="menuVisible = true"
+		@mouseleave="menuVisible = false"
+	>
+		<button class="select">
+			<span class="icon">
+				<span class="language select-icon"></span>
+				<span class="arrow select-icon"></span>
+			</span>
+		</button>
+		<Transition name="menu">
+			<div class="menu" v-if="menuVisible">
+				<SelectMenu :list="menu" v-model:current="current"></SelectMenu>
+			</div>
+		</Transition>
+	</div>
 </template>
 
 <style scoped>
 .wrapper {
-  position: relative;
-  display: flex;
-  flex-direction: column;
+	position: relative;
+	display: flex;
+	flex-direction: column;
 
-  height: 100%;
+	height: 100%;
 }
 /*#region Button */
 .select {
-  display: flex;
-  align-items: center;
-  height: 100%;
+	display: flex;
+	align-items: center;
+	height: 100%;
+	padding: 0 12px;
 
-  color: var(--text-color-1);
-  border: none;
-  background-color: transparent;
-  background-image: none;
-  transition: color 0.5s;
+	color: var(--text-color-1);
+	border: none;
+	background-color: transparent;
+	background-image: none;
+	transition: color 0.5s;
 }
 .wrapper:hover > .select {
-  color: var(--text-color-2);
-  transition: color 0.25s;
+	color: var(--text-color-2);
+	transition: color 0.25s;
 }
 .icon {
-  width: 34px;
-  display: flex;
-  align-items: center;
-  gap: 4px;
+	width: 34px;
+	display: flex;
+	align-items: center;
+	gap: 4px;
 }
 .select-icon {
-  background-color: currentColor;
-  color: inherit;
+	background-color: currentColor;
+	color: inherit;
 }
 .language {
-  mask: url("/img/language.svg") no-repeat;
-  width: 16px;
-  height: 16px;
+	mask: url("/img/language.svg") no-repeat;
+	width: 16px;
+	height: 16px;
 }
 .arrow {
-  mask: url("/img/arrow.svg") no-repeat;
-  width: 14px;
-  height: 14px;
-  transform: rotate(90deg);
+	mask: url("/img/arrow.svg") no-repeat;
+	width: 14px;
+	height: 14px;
+	transform: rotate(90deg);
 }
 /*#endregion */
 /*#region Menu */
 .menu {
-  position: absolute;
-  right: -12px;
-  top: calc(100% / 2 + 20px);
+	position: absolute;
+	right: -12px;
+	top: calc(100% / 2 + 20px);
 }
 .menu-enter-active,
 .menu-leave-active {
-  transition: opacity 0.25s ease;
+	transition: opacity 0.25s ease;
 }
 
 .menu-enter-from,
 .menu-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 /*#endregion */
 </style>
--- services/landing/src/components/SelectMenu.vue
+++ services/landing/src/components/SelectMenu.vue
@@ -1,51 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+	list: {
+		type: Array<string>,
+		required: true,
+	},
+});
+const current = defineModel("current", {
+	type: Number,
+	required: true,
+});
+</script>
 
 <template>
-  <div class="wrapper">
-    <ul class="menu">
-      <li>English</li>
-      <li>Русский</li>
-    </ul>
-  </div>
+	<div class="wrapper">
+		<ul class="menu">
+			<li
+				:class="{ current: current === index }"
+				v-for="(record, index) in props.list"
+			>
+				{{ record }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <style scoped>
 .wrapper {
-  display: flex;
-  align-items: start;
-  justify-content: center;
+	display: flex;
+	align-items: start;
+	justify-content: center;
 
-  padding: 12px;
-  min-width: 128px;
-  max-height: 200px;
+	padding: 12px;
+	min-width: 128px;
+	max-height: 200px;
 
-  border: 1px solid var(--divider-color);
-  border-radius: 12px;
-  background-color: var(--menu-bg);
-  box-shadow: var(--vp-shadow-3);
-  transition: background-color 0.5s;
+	border: 1px solid var(--divider-color);
+	border-radius: 12px;
+	background-color: var(--menu-bg);
+	box-shadow: var(--vp-shadow-3);
+	transition: background-color 0.5s;
 }
 .menu {
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  justify-content: start;
+	display: flex;
+	flex-direction: column;
+	align-items: start;
+	justify-content: start;
 
-  list-style: none;
+	list-style: none;
 
-  padding: 0;
-  margin: 0;
+	padding: 0;
+	margin: 0;
 }
 .menu li {
-  font-size: 14px;
-  font-weight: 500;
-  color: var(--text-color-1);
+	font-size: 14px;
+	color: var(--text-color-1);
 
-  padding: 0 24px 0 12px;
-  line-height: 32px;
-  white-space: nowrap;
+	border-radius: 6px;
+	padding: 0 24px 0 12px;
+	line-height: 32px;
+	white-space: nowrap;
 }
-.menu li:first-child {
-  font-weight: 700;
+.menu li:not(.current) {
+	font-weight: 500;
+	cursor: pointer;
+	transition: background-color 0.25s, color 0.25s;
+}
+.menu li:not(.current):hover {
+	color: var(--highlight-color-1);
+	background-color: var(--input-switch-bg-color);
+	transition: background-color 0.25s, color 0.25s;
+}
+.current {
+	font-weight: 700;
 }
 </style>
