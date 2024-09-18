87c1f83b094db5a449442ee4043ccaf9eb36e9d3
infanasotku <infanasotku@gmail.com>
2024-09-18 15:07:02 +0700

    Added content block

---
+++ services/landing/src/components/ContentBlock.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<div class="block">
+		<h1>{{ props.title }}</h1>
+		<slot></slot>
+	</div>
+</template>
+
+<style scoped>
+.block {
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+
+	font-size: 16px;
+	line-height: 24px;
+	font-weight: 400;
+}
+
+h1 {
+	line-height: 40px;
+	font-size: 32px;
+	font-weight: 600;
+	margin: 0;
+}
+</style>
