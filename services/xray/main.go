8a66247e513533bbaeb0ddb4d7b01d7b893ff424
infanasotku <infanasotku@gmail.com>
2024-09-19 20:15:58 +0700

    Added greet arg

--- services/xray/main.go
+++ services/xray/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -8,12 +9,41 @@ import (
 
 	"github.com/infanasotku/netku/services/xray/gen"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health"
 	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
 	ConfigureEnvs()
+
+	// Health check mode.
+	if len(os.Args) > 1 && os.Args[1] == "--greet" {
+		os.Exit(greet())
+	}
+
+	serve()
+}
+
+func greet() int {
+	port := os.Getenv("XRAY_PORT")
+
+	conn, err := grpc.NewClient(fmt.Sprintf("localhost:%s", port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return 1
+	}
+	defer conn.Close()
+
+	checker := healthgrpc.NewHealthClient(conn)
+	_, err = checker.Check(context.Background(), &healthgrpc.HealthCheckRequest{Service: "xray"})
+
+	if err != nil {
+		return 1
+	}
+	return 0
+}
+
+func serve() {
 	port := os.Getenv("XRAY_PORT")
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
