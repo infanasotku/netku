d6a1eb6f6f55fa4cb0b5a43839209392e2637172
infanasotku <infanasotku@gmail.com>
2024-09-25 22:27:20 +0700

    moved id to Base model

--- services/assistant/db/database.py
+++ services/assistant/db/database.py
@@ -1,4 +1,6 @@
+from typing import Annotated
 from sqlalchemy import create_engine
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from settings import settings
 
@@ -7,6 +9,8 @@ engine = create_engine(settings.psql_dsn)
 
 session = sessionmaker(engine)
 
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[intpk]
