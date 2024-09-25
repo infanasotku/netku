933d52aadac26460ea6b784dbc6d80081d38ca40
infanasotku <infanasotku@gmail.com>
2024-09-25 09:25:24 +0700

    Added new settings for booking service/Renamed old vars

--- services/assistant/settings.py
+++ services/assistant/settings.py
@@ -27,7 +27,11 @@ class Settings(BaseSettings):
     xray_restart_minutes: float = Field(validation_alias="XRAY_RESTART_MINUTES")
     xray_port: int = Field(validation_alias="XRAY_PORT", default=9000)
     xray_host: str = Field(validation_alias="XRAY_HOST", default="127.0.0.1")
+    # endregion
 
+    # region Booking
+    booking_port: int = Field(validation_alias="BOOKING_PORT", default=9001)
+    booking_host: str = Field(validation_alias="BOOKING_HOST", default="127.0.0.1")
     # endregion
 
     # region Bot
