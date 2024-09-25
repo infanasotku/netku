9f2fa1577a65b0129b1b5c5b9c77ce508dbecfe1
infanasotku <infanasotku@gmail.com>
2024-09-25 22:33:58 +0700

    Added booking accounts table

---
+++ services/assistant/alembic/versions/c324fbce330b_adding_booking_account.py
@@ -0,0 +1,40 @@
+"""Adding booking account
+
+Revision ID: c324fbce330b
+Revises: 6b330bf463fa
+Create Date: 2024-09-25 22:32:07.894266
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c324fbce330b"
+down_revision: Union[str, None] = "6b330bf463fa"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "booking_accounts",
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.Column("owner_id", sa.Integer(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["owner_id"],
+            ["users.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+        sa.UniqueConstraint("password"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("booking_accounts")
--- services/assistant/db/models.py
+++ services/assistant/db/models.py
@@ -1,6 +1,6 @@
 from db.database import Base
-from sqlalchemy import BigInteger
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 
 class User(Base):
@@ -11,3 +11,17 @@ class User(Base):
 
     # Subscriptions
     proxy_subscription: Mapped[bool] = mapped_column(default=False, nullable=False)
+
+    booking_accounts: Mapped[list["BookingAccount"]] = relationship(
+        "BookingAccount", back_populates="owner"
+    )
+
+
+class BookingAccount(Base):
+    __tablename__ = "booking_accounts"
+
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship("User", back_populates="booking_accounts")
