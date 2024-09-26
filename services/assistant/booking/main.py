992afee0dc9d8a848781cb8ac9d5e8f37b71aaad
infanasotku <infanasotku@gmail.com>
2024-09-26 10:39:14 +0700

    Added booked status

--- services/assistant/booking/main.py
+++ services/assistant/booking/main.py
@@ -8,6 +8,11 @@ from booking.gen.booking_pb2 import BookingRequest
 
 
 class Booking:
+    def __init__(self):
+        # Displayed booking status email: status
+        # Machine is booked if status = True
+        self._statuses: dict[str, bool] = {}
+
     async def run_booking(self, email: str, password: str):
         if not await health.wait_healthy(
             "booking", f"{settings.booking_host}:{settings.booking_port}"
@@ -19,6 +24,7 @@ class Booking:
         ) as ch:
             stub = BookingStub(ch)
             await stub.RunBooking(BookingRequest(email=email, password=password))
+            self._statuses[email] = True
 
     async def stop_booking(self, email: str, password: str):
         if not await health.wait_healthy(
@@ -31,3 +37,8 @@ class Booking:
         ) as ch:
             stub = BookingStub(ch)
             await stub.StopBooking(BookingRequest(email=email, password=password))
+            self._statuses[email] = False
+
+    def booked(self, email: str) -> bool:
+        """Returns booking status, `True` if machine booked, `False` otherwise"""
+        return self._statuses.get(email, False)
