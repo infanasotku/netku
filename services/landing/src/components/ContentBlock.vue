23f9bd994c2b68d2596c52e97149cbcb3dda8cc2
infanasotku <infanasotku@gmail.com>
2024-10-07 15:04:44 +0700

    Added code style

--- services/landing/src/components/ContentBlock.vue
+++ services/landing/src/components/ContentBlock.vue
@@ -64,4 +64,15 @@ const props = defineProps({
 .content-block a:active {
 	color: var(--highlight-color-lightest);
 }
+
+.content-block :not(pre) > code {
+	border-radius: 4px;
+	padding: 3px 6px;
+	background-color: var(--code-bg);
+	transition: color 0.25s, background-color 0.5s;
+}
+
+.content-block :not(pre, h1, h2, h3, h4, h5, h6) > code {
+	color: var(--code-color);
+}
 </style>
