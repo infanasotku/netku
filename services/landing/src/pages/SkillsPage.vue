3d1d2309cbfd5c66b5498455f1cf6a4418aa8c7a
infanasotku <infanasotku@gmail.com>
2024-10-09 15:33:42 +0700

    Started work with skills page

---
+++ services/landing/src/pages/SkillsPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ContentBlock from "@/components/ContentBlock.vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+const localt = (key: string) => t("content.skills." + key);
+</script>
+
+<template>
+	<main>
+		<ContentBlock :title="localt('title')">
+			<i18n-t scope="global" keypath="content.skills.description" tag="p">
+			</i18n-t scope="global">
+		</ContentBlock>
+		<slot></slot>
+	</main>
+</template>
+
+<style scoped></style>
