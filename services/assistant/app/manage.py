08d63f89925eb827bf679771a6a3a5188144b868
infanasotku <infanasotku@gmail.com>
2024-10-17 16:49:09 +0700

    Added entry point/Corrected sql driver

---
+++ services/assistant/app/manage.py
@@ -0,0 +1,50 @@
+import logging
+import pathlib
+import sys
+import uvicorn
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+sys.path.append((pathlib.Path(__file__).parent.parent).resolve().as_posix())
+from settings import settings, logger
+from app import AppFactory
+from bot import BotFactory, BotServiceFactories, BotSettings
+from database import get_db_factory, ServiceFactory
+
+
+def run():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname)s:     [%(asctime)s] %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+    )
+    log_config_path = settings.app_directory_path + "/log_config.yaml"
+
+    # Initting app
+    async_engine = create_async_engine(settings.psql_dsn)
+    async_session = async_sessionmaker(async_engine)
+    get_db = get_db_factory(async_session)
+    service_factories = ServiceFactory(get_db)
+
+    # bot
+    bot_factory = BotFactory(
+        bot_settings=BotSettings(**settings.model_dump()),
+        bot_service_factories=BotServiceFactories(
+            user_service_factory=service_factories.user_service_factory
+        ),
+        logger=logger,
+    )
+
+    factory = AppFactory(bot_factory)
+
+    uvicorn.run(
+        app=factory.create_app(),
+        host=settings.host,
+        port=settings.port,
+        log_config=log_config_path,
+        ssl_keyfile=settings.ssl_keyfile,
+        ssl_certfile=settings.ssl_certfile,
+    )
+
+
+if __name__ == "__main__":
+    run()
