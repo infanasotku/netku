d942a9607506448579e6b9c1c3c99c89a8a9da5d
infanasotku <infanasotku@gmail.com>
2024-10-17 16:45:27 +0700

    Added base app factory

---
+++ services/assistant/app/app.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Callable
+from fastapi import FastAPI
+
+
+class AbstractAppFactory(ABC):
+    route_path: str
+
+    @abstractmethod
+    def create_app(self) -> FastAPI:
+        """Creates fastapi app."""
+        pass
+
+    def create_lifespan(self) -> Callable[[FastAPI], AsyncGenerator]:
+        """Creates fastapi lifespan."""
+        pass
+
+
+class AppFactory(AbstractAppFactory):
+    def __init__(self, *sub_factories: AbstractAppFactory):
+        """Inits main app.
+        :param get_db: DB async session factory.
+        """
+        self.sub_factories = sub_factories
+
+    def create_app(self) -> FastAPI:
+        app = FastAPI(
+            lifespan=self.create_lifespan(),
+            docs_url=None,
+            redoc_url=None,
+        )
+
+        return app
+
+    def create_lifespan(self) -> Callable[[FastAPI], AsyncGenerator]:
+        """Creates core lifespan which handle `lifespans` of all app."""
+        sub_lifespans = [
+            sub_factory.create_lifespan() for sub_factory in self.sub_factories
+        ]
+
+        @asynccontextmanager
+        async def lifespan(app: FastAPI) -> AsyncGenerator:
+            generators = [lifespan(app) for lifespan in sub_lifespans]
+            for generator in generators:
+                await anext(generator)
+            yield
+            for generator in generators:
+                await anext(generator)
+
+        return lifespan
