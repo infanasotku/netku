1f3ba4e9b69c8b36618ad9cd94127abc2a0e709b
infanasotku <infanasotku@gmail.com>
2024-10-22 22:28:47 +0700

    Added first test

---
+++ services/assistant/tests/stubs.py
@@ -0,0 +1,29 @@
+from typing import Any, Optional, Type
+from app.database.orm import AbstractRepository
+from app.database.database import Base
+from app.database.models import XrayRecord
+from app.database.schemas import UserSchema
+
+
+class StubRepository(AbstractRepository):
+    async def find_first(
+        self, model: Type[Base], column: Any, value: Any
+    ) -> Base | None:
+        raise NotImplementedError
+
+    async def get_all(self, model: Type[Base]) -> list[Base]:
+        raise NotImplementedError
+
+    async def update_user(self, user: UserSchema) -> bool:
+        raise NotImplementedError
+
+    async def create_booking_account(
+        self, user: UserSchema, email: str, password: str
+    ) -> bool:
+        raise NotImplementedError
+
+    async def update_xray_record(self, uid: str) -> None:
+        raise NotImplementedError
+
+    async def get_xray_record(self) -> Optional[XrayRecord]:
+        raise NotImplementedError
