f9fcbda79e771dc11ba8537807bdc36d05be7a27
infanasotku <infanasotku@gmail.com>
2024-10-22 19:30:44 +0700

    Deleted deprecated Config style

--- services/assistant/app/database/schemas.py
+++ services/assistant/app/database/schemas.py
@@ -1,10 +1,9 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class BaseSchema(BaseModel):
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
     id: Optional[int] = -1
 
