fdc86bd3d132cdd8cf7a75ae622b3d87cc81bd98
infanasotku <infanasotku@gmail.com>
2024-11-10 18:28:53 +0700

    Added tools switch

--- services/landing/src/components/HeadBar.vue
+++ services/landing/src/components/HeadBar.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import ThemeSwitch from "@/components/ThemeSwitch.vue";
 import LinkIcon from "@/components/LinkIcon.vue";
 import LanguageSelect from "@/components/LanguageSelect.vue";
 import NetkuLogo from "@/components/NetkuLogo.vue";
 
 const emits = defineEmits(["menu-click"]);
+
+const popUpTools = ref(false);
 </script>
 
 <template>
@@ -13,11 +16,14 @@ const emits = defineEmits(["menu-click"]);
       <span @click.prevent="emits('menu-click')" class="menu-icon"></span>
       <NetkuLogo :withLabel="false" class="logo"></NetkuLogo>
     </div>
+    <div class="tool-switch">
+      <span class="icon"></span>
+    </div>
     <div class="tools">
       <div class="divider"></div>
       <LanguageSelect></LanguageSelect>
       <div class="divider"></div>
-      <ThemeSwitch class="swicth"></ThemeSwitch>
+      <ThemeSwitch class="switch"></ThemeSwitch>
       <div class="divider"></div>
       <div class="social-icons">
         <LinkIcon
@@ -30,6 +36,9 @@ const emits = defineEmits(["menu-click"]);
         ></LinkIcon>
       </div>
     </div>
+    <Transition name="tools">
+      <div v-if="popUpTools" class="pop-up-tools pop-up"></div>
+    </Transition>
   </header>
 </template>
 
@@ -41,7 +50,7 @@ const emits = defineEmits(["menu-click"]);
 .headbar {
   background-color: var(--bg-block);
 
-  height: 64px;
+  height: 65px;
 
   display: flex;
   flex-grow: 1;
@@ -50,75 +59,114 @@ const emits = defineEmits(["menu-click"]);
   align-items: center;
 
   border-bottom: 1px solid var(--divider-color);
-}
 
-/*#region menu */
-.menu {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  .menu {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
-  gap: 10px;
-  opacity: 0;
-}
+    gap: 10px;
+    opacity: 0;
 
-.menu-icon {
-  background-color: currentColor;
-  color: inherit;
-  width: 20px;
-  height: 20px;
-  fill: currentColor;
-  mask: url("/img/menu.svg") no-repeat;
+    .menu-icon {
+      background-color: currentColor;
+      color: inherit;
+      width: 20px;
+      height: 20px;
+      fill: currentColor;
+      mask: url("/img/menu.svg") no-repeat;
 
-  color: var(--text-color-2);
-  transition: color 0.25s;
-}
+      color: var(--text-color-2);
+      transition: color 0.25s;
 
-.menu-icon:hover {
-  color: var(--text-color-1);
-  cursor: pointer;
-}
+      &:hover {
+        color: var(--text-color-1);
+        cursor: pointer;
+      }
+    }
 
-.logo {
-  height: 63px;
-}
+    .logo {
+      height: 63px;
+    }
+  }
 
-/*#endregion */
+  .tool-switch {
+    align-items: center;
+    position: relative;
+    padding: 0 12px;
+    margin-right: -12px;
+    height: 100%;
 
-/*#region tools */
-.tools {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+    &:hover {
+      cursor: pointer;
+    }
 
-  height: 100%;
+    .icon {
+      color: var(--text-color-1);
+      background-color: currentColor;
+      mask: url("/img/ellipsis.svg") no-repeat;
+
+      height: 20px;
+      width: 20px;
+    }
+
+    display: none;
+  }
+
+  .tools {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    height: 100%;
+
+    .social-icons {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .divider {
+      width: 1px;
+      height: 24px;
+      background-color: var(--divider-color);
+      margin: 0 8px;
+    }
+
+    .switch {
+      margin-left: 8px;
+      margin-right: 8px;
+    }
+  }
+
+  .tools-enter-active,
+  .tools-leave-active {
+    transition: opacity 0.25s ease;
+  }
+
+  .tools-enter-from,
+  .tools-leave-to {
+    opacity: 0;
+  }
+
+  @include media-breakpoint-down(lg) {
+    .menu {
+      opacity: 1;
+    }
+    .tool-switch {
+      display: flex;
+    }
+    .tools {
+      display: none;
+    }
+  }
+  @include media-breakpoint-up(lg) {
+    .pop-up-tools {
+      display: none;
+    }
+  }
 }
 
 .dark .headbar {
   border-bottom: 1px solid #000000;
-}
-
-.social-icons {
-  display: flex;
-  flex-direction: row;
-}
-
-.divider {
-  width: 1px;
-  height: 24px;
-  background-color: var(--divider-color);
-  margin: 0 8px;
-}
-
-.switch {
-  margin-left: 8px;
-  margin-right: 8px;
-}
-/*#endregion */
-
-@include media-breakpoint-down(lg) {
-  .menu {
-    opacity: 1;
-  }
 }
 </style>
