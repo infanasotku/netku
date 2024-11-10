0b55a8aadd4d07de77fb31170b00a3c812ce083e
infanasotku <infanasotku@gmail.com>
2024-11-10 15:49:31 +0700

    Separated list wrapper from select menu

--- services/landing/src/components/LanguageSelect.vue
+++ services/landing/src/components/LanguageSelect.vue
@@ -41,74 +41,79 @@ const current = computed({
       </span>
     </button>
     <Transition name="menu">
-      <div class="menu" v-if="menuVisible">
+      <div class="menu drop-down" v-if="menuVisible">
         <SelectMenu :list="menu" v-model:current="current"></SelectMenu>
       </div>
     </Transition>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .wrapper {
   position: relative;
   display: flex;
   flex-direction: column;
 
   height: 100%;
-}
-/*#region Button */
-.select {
-  display: flex;
-  align-items: center;
-  height: 100%;
-  padding: 0 12px;
 
-  color: var(--text-color-1);
-  border: none;
-  background-color: transparent;
-  background-image: none;
-  transition: color 0.5s;
-}
-.wrapper:hover > .select {
-  color: var(--text-color-2);
-  transition: color 0.25s;
-}
-.icon {
-  width: 34px;
-  display: flex;
-  align-items: center;
-  gap: 4px;
-}
-.select-icon {
-  background-color: currentColor;
-  color: inherit;
-}
-.language {
-  mask: url("/img/language.svg") no-repeat;
-  width: 16px;
-  height: 16px;
-}
-.arrow {
-  mask: url("/img/arrow.svg") no-repeat;
-  width: 14px;
-  height: 14px;
-  transform: rotate(90deg);
-}
-/*#endregion */
-/*#region Menu */
-.menu {
-  position: absolute;
-  right: -12px;
-  top: calc(100% / 2 + 20px);
-}
-.menu-enter-active,
-.menu-leave-active {
-  transition: opacity 0.25s ease;
-}
+  .select {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 12px;
 
-.menu-enter-from,
-.menu-leave-to {
-  opacity: 0;
+    color: var(--text-color-1);
+    border: none;
+    background-color: transparent;
+    background-image: none;
+    transition: color 0.5s;
+
+    .icon {
+      width: 34px;
+      display: flex;
+      align-items: center;
+      gap: 4px;
+
+      .select-icon {
+        background-color: currentColor;
+        color: inherit;
+      }
+
+      .language {
+        mask: url("/img/language.svg") no-repeat;
+        width: 16px;
+        height: 16px;
+      }
+      .arrow {
+        mask: url("/img/arrow.svg") no-repeat;
+        width: 14px;
+        height: 14px;
+        transform: rotate(90deg);
+      }
+    }
+  }
+
+  &:hover {
+    .select {
+      color: var(--text-color-2);
+      transition: color 0.25s;
+    }
+  }
+
+  .menu {
+    position: absolute;
+    right: -12px;
+    top: calc(100% / 2 + 20px);
+  }
+
+  .menu-enter-active,
+  .menu-leave-active {
+    transition: opacity 0.25s ease;
+  }
+
+  .menu-enter-from,
+  .menu-leave-to {
+    opacity: 0;
+  }
 }
-/*#endregion */
 </style>
--- services/landing/src/components/SelectMenu.vue
+++ services/landing/src/components/SelectMenu.vue
@@ -19,33 +19,16 @@ const list = computed(() => {
 </script>
 
 <template>
-  <div class="wrapper">
-    <ul class="menu">
-      <li class="current">{{ props.list[current] }}</li>
-      <li v-for="record in list" @click="current = record.index">
-        <span class="text">{{ record.text }}</span>
-      </li>
-    </ul>
-  </div>
+  <ul class="menu-list">
+    <li class="current">{{ props.list[current] }}</li>
+    <li v-for="record in list" @click="current = record.index">
+      <span class="text">{{ record.text }}</span>
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-.wrapper {
-  display: flex;
-  align-items: start;
-  justify-content: center;
-
-  padding: 12px;
-  min-width: 128px;
-  max-height: 200px;
-
-  border: 1px solid var(--divider-color);
-  border-radius: 12px;
-  background-color: var(--menu-bg);
-  box-shadow: var(--vp-shadow-3);
-  transition: background-color 0.5s;
-}
-.menu {
+<style scoped lang="scss">
+.menu-list {
   display: flex;
   flex-direction: column;
   align-items: start;
@@ -55,52 +38,59 @@ const list = computed(() => {
 
   padding: 0;
   margin: 0;
-}
-.menu li {
-  font-size: 14px;
-  color: var(--text-color-1);
 
-  border-radius: 6px;
-  padding: 0 24px 0 12px;
-  line-height: 32px;
-  white-space: nowrap;
-}
-.menu li:not(.current) {
-  font-weight: 500;
-  cursor: pointer;
-  transition:
-    background-color 0.25s,
-    color 0.25s;
-}
-.menu li:not(.current):hover {
-  color: var(--highlight-color-lighter);
-  background-color: var(--input-switch-bg-color);
-  transition:
-    background-color 0.25s,
-    color 0.25s;
-}
-.current {
-  font-weight: 700;
-}
-.text {
-  display: flex;
-  align-items: center;
+  li {
+    font-size: 14px;
+    color: var(--text-color-1);
 
-  gap: 6px;
-}
-.menu li:not(.current) .text::after {
-  content: "";
+    border-radius: 6px;
+    padding: 0 24px 0 12px;
+    line-height: 32px;
+    white-space: nowrap;
 
-  width: 16px;
-  height: 16px;
+    &:not(.current) {
+      font-weight: 500;
+      cursor: pointer;
+      transition:
+        background-color 0.25s,
+        color 0.25s;
 
-  transition: transform 0.25s;
-  color: var(--text-color-3);
-  background-color: currentColor;
-  mask: url("/img/arrow.svg") no-repeat;
-}
-.menu li:not(.current):hover .text::after {
-  transition: transform 0.25s;
-  transform: translateX(6px);
+      &:hover {
+        color: var(--highlight-color-lighter);
+        background-color: var(--input-switch-bg-color);
+        transition:
+          background-color 0.25s,
+          color 0.25s;
+
+        .text::after {
+          transition: transform 0.25s;
+          transform: translateX(6px);
+        }
+      }
+
+      .text::after {
+        content: "";
+
+        width: 16px;
+        height: 16px;
+
+        transition: transform 0.25s;
+        color: var(--text-color-3);
+        background-color: currentColor;
+        mask: url("/img/arrow.svg") no-repeat;
+      }
+    }
+
+    &.current {
+      font-weight: 700;
+    }
+
+    .text {
+      display: flex;
+      align-items: center;
+
+      gap: 6px;
+    }
+  }
 }
 </style>
