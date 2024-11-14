daab6d89ba33ff728ccd9e11d017f8cc7f0027c4
infanasotku <infanasotku@gmail.com>
2024-11-14 21:45:01 +0700

    Changed logo and logo component

--- services/landing/src/components/NetkuLogo.vue
+++ services/landing/src/components/NetkuLogo.vue
@@ -10,13 +10,13 @@ const props = defineProps({
 <template>
   <div class="logo">
     <a href="/">
-      <img style="width: 25px" src="/img/netku.svg" />
-      <span v-if="props.withLabel">Netku</span>
+      <span class="icon"></span>
+      <span class="title" v-if="props.withLabel">Netku</span>
     </a>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .logo {
   width: fit-content;
   height: 64px;
@@ -31,14 +31,29 @@ const props = defineProps({
 
   z-index: 1;
   background-color: inherit;
-}
-.logo a {
-  display: flex;
-  gap: 10px;
-}
-.logo span {
-  font-size: 16px;
-  font-weight: 600;
-  color: var(--text-color-1);
+
+  a {
+    display: flex;
+    gap: 10px;
+
+    .title {
+      font-size: 16px;
+      font-weight: 600;
+      color: var(--text-color-1);
+    }
+
+    .icon {
+      display: flex;
+
+      background-color: currentColor;
+      width: 25px;
+      height: 25px;
+      fill: currentColor;
+      mask: url("/img/netku.svg") no-repeat;
+
+      color: var(--text-color-1);
+      transition: color 0.25s;
+    }
+  }
 }
 </style>
