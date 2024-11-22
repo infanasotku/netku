7b69b27df815074f653cec106ef840b6fce8af4e
infanasotku <infanasotku@gmail.com>
2024-11-22 21:17:45 +0700

    Implemented user repo for sqlaclhemy

---
+++ services/assistant/app/database/repositories/user_repository.py
@@ -0,0 +1,66 @@
+from sqlalchemy import select
+from sqlalchemy.orm import InstrumentedAttribute
+
+from app.repositories.user_repository import UserRepository
+
+from app.database.repositories.base_repository import BaseRepository
+from app.schemas.user_schemas import UserCreateSchema, UserSchema, UserUpdateSchema
+
+from app.database import converters
+from app.database.models import User
+from app.database.orm import selectinload_all
+
+
+class SQLUserRepository(UserRepository, BaseRepository):
+    async def _get_user_model_by_column(
+        self, column: InstrumentedAttribute, value
+    ) -> User | None:
+        s = select(User).options(*selectinload_all(User)).filter(column == value)
+        return (await self.session.execute(s)).scalars().first()
+
+    async def get_user_by_id(self, id: int) -> UserSchema | None:
+        user = await self._get_user_model_by_column(User.id, id)
+
+        if user is None:
+            return None
+
+        return converters.user_to_user_schema(user)
+
+    async def get_user_by_telegram_id(self, telegram_id: int) -> UserSchema | None:
+        user = await self._get_user_model_by_column(User.telegram_id, telegram_id)
+
+        if user is None:
+            return None
+
+        return converters.user_to_user_schema(user)
+
+    async def get_all_users(self) -> list[UserSchema]:
+        s = select(User).options(*selectinload_all(User))
+
+        users = (await self.session.execute(s)).scalars().all()
+
+        return [converters.user_to_user_schema(user) for user in users]
+
+    async def create_user(self, user_create: UserCreateSchema) -> UserSchema:
+        user = converters.user_create_schema_to_user(user_create)
+        self.session.add(user)
+        await self.session.flush()
+        await self.session.refresh(user)
+
+        return converters.user_to_user_schema(user)
+
+    async def update_user(
+        self, user_id: int, user_update: UserUpdateSchema
+    ) -> UserSchema:
+        user = await self._get_user_model_by_column(User.id, user_id)
+
+        if user is None:
+            raise Exception(f"User with id {user_id} not exist.")
+
+        for field, value in user_update.model_dump(exclude_unset=True).items():
+            setattr(user, field, value)
+
+        await self.session.flush()
+        await self.session.refresh(user)
+
+        return converters.user_to_user_schema(user)
--- services/assistant/app/schemas/user_schemas.py
+++ services/assistant/app/schemas/user_schemas.py
@@ -1,12 +1,20 @@
-from app.schemas.schema import BaseSchema
+from app.schemas.base_schema import BaseSchema
 from app.schemas.booking_schemas import BookingAccountSchema
 
 
-class UserSchema(BaseSchema):
+class UserUpdateSchema(BaseSchema):
+    phone_number: str | None = None
+    telegram_id: int | None = None
+    proxy_subscription: bool | None = None
+
+
+class UserCreateSchema(BaseSchema):
     phone_number: str | None
     telegram_id: int | None
 
     # Subscriptions
     proxy_subscription: bool
 
+
+class UserSchema(UserCreateSchema):
     booking_accounts: list[BookingAccountSchema] = []
