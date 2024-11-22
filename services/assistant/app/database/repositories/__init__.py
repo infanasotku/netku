0340eeafe18d66fc273963e572a6387cb1185b3b
infanasotku <infanasotku@gmail.com>
2024-11-22 21:18:25 +0700

    Added missed base repo

---
+++ services/assistant/app/database/repositories/__init__.py
@@ -0,0 +1,4 @@
+from app.database.repositories.booking_repository import SQLBookingRepository
+from app.database.repositories.user_repository import SQLUserRepository
+
+__all__ = ["SQLBookingRepository", "SQLUserRepository"]
