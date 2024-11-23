4407da8505dd321d9b63fa68dd2a9e51726c5a74
infanasotku <infanasotku@gmail.com>
2024-11-23 12:54:39 +0700

    Added repo factory

---
+++ services/assistant/app/database/repositories/repository_factory.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Callable, Type, TypeVar, AsyncContextManager
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.repositories.base_repository import SQLBaseRepository
+
+
+class SQLRepositoryFactory:
+    SQLRepositoryT = TypeVar("SQLRepositoryT", bound=SQLBaseRepository)
+
+    def __init__(
+        self,
+        get_db: Callable[[], AsyncContextManager[AsyncSession]],
+        repository_type: Type[SQLRepositoryT],
+    ):
+        self.get_db = get_db
+        self._Repo = repository_type
+
+    @asynccontextmanager
+    async def create(self) -> AsyncGenerator[SQLRepositoryT, None]:
+        async with self.get_db() as session:
+            yield self._Repo(session)
