3acdc60ebd1ae5c1cfe356c3a829c66614a7dfbb
infanasotku <infanasotku@gmail.com>
2024-11-24 15:11:26 +0700

    Added booking analysis service

--- services/assistant/app/contracts/services/__init__.py
+++ services/assistant/app/contracts/services/__init__.py
@@ -2,8 +2,6 @@ from app.contracts.services.user import UserService
 from app.contracts.services.booking import BookingService
 from app.contracts.services.xray import XrayService
 
-__all__ = [
-    "UserService",
-    "BookingService",
-    "XrayService",
-]
+from app.contracts.services.analysis.user_booking import BookingAnalysisService
+
+__all__ = ["UserService", "BookingService", "XrayService", "BookingAnalysisService"]
