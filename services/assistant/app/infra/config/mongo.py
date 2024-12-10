58fcf4e1c14d636615d37d5a9138d84a122b7e96
infanasotku <infanasotku@gmail.com>
2024-12-10 12:18:37 +0700

    Changed env name

--- services/assistant/app/infra/config/mongo.py
+++ services/assistant/app/infra/config/mongo.py
@@ -3,7 +3,7 @@ from pydantic import Field, computed_field
 
 
 class MongoSettings(BaseSettings):
-    mongo_pass: str = Field(validation_alias="MONGO_PASS")
+    mongo_pass: str = Field(validation_alias="MONGO_PASSWORD")
     mongo_user: str = Field(validation_alias="MONGO_USER")
     mongo_host: str = Field(validation_alias="MONGO_HOST", default="127.0.0.1")
     mongo_port: int = Field(validation_alias="MONGO_PORT", default=27017)
