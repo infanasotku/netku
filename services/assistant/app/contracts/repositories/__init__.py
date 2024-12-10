baf49014e3a33a81a5817836c96baa84109fbc07
infanasotku <infanasotku@gmail.com>
2024-12-10 18:10:17 +0700

    Added repo to init side

--- services/assistant/app/contracts/repositories/__init__.py
+++ services/assistant/app/contracts/repositories/__init__.py
@@ -1,5 +1,11 @@
 from app.contracts.repositories.booking import BookingRepository
 from app.contracts.repositories.xray import XrayRepository
 from app.contracts.repositories.user import UserRepository
+from app.contracts.repositories.availability import AvailabilityRepository
 
-__all__ = ["BookingRepository", "XrayRepository", "UserRepository"]
+__all__ = [
+    "BookingRepository",
+    "XrayRepository",
+    "UserRepository",
+    "AvailabilityRepository",
+]
