d9dd2afdf3c7de8bce36b53fbfc543a269278eb5
infanasotku <infanasotku@gmail.com>
2024-12-10 15:02:35 +0700

    Described method for count service statistic

--- services/assistant/app/contracts/repositories/availability.py
+++ services/assistant/app/contracts/repositories/availability.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
-from app.schemas.availability import AvailabilitySchema, AvailabilityCreateSchema
+from app.schemas.availability import (
+    AvailabilitySchema,
+    AvailabilityCreateSchema,
+    Service,
+)
 
 
 class AvailabilityRepository(ABC):
@@ -11,4 +16,14 @@ class AvailabilityRepository(ABC):
         """Logs availability record to DB.
 
         :return: Logged availability row.
+        """
+
+    @abstractmethod
+    async def get_service_availability_factor_by_period(
+        self, service_name: Service, start: datetime, end: datetime
+    ) -> float:
+        """Counts average availability factor for service
+        for period from `start` to `end` in DB.
+
+        :return: Counted factor.
         """
