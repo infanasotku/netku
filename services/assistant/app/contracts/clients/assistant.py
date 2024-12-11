0b398a5159491eee49c043abec6783af83bc2836
infanasotku <infanasotku@gmail.com>
2024-12-11 22:48:03 +0700

    Remove extra

--- services/assistant/app/contracts/clients/assistant.py
+++ services/assistant/app/contracts/clients/assistant.py
@@ -1,11 +1,7 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from app.contracts.clients.base import BaseClient
 
 
 class AssistantClient(BaseClient, ABC):
-    @abstractmethod
-    async def check_health(self) -> bool:
-        """Checks health of assistant service.
-
-        :return: `True` if assistant available, `False` otherwise."""
+    pass
