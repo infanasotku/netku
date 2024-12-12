557cc93c910403549c2e8243db1a490251e91d29
infanasotku <infanasotku@gmail.com>
2024-12-12 15:02:09 +0700

    Return missed param

--- services/assistant/app/contracts/clients/telegram.py
+++ services/assistant/app/contracts/clients/telegram.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class TelegramClient(ABC):
     @abstractmethod
-    async def send_message(message: str, id: int):
+    async def send_message(self, message: str, id: int):
         """Sends message to user by his id."""
