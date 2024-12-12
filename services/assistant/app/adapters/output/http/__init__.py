c989bdb624b81c01702608b41cd2fb4949e91704
infanasotku <infanasotku@gmail.com>
2024-12-12 16:34:50 +0700

    Forward clients to http level

---
+++ services/assistant/app/adapters/output/http/__init__.py
@@ -0,0 +1,7 @@
+from app.adapters.output.http.telegram import HTTPTelegramClient
+from app.adapters.output.http.assistant import HTTPAssistantlient
+
+__all__ = [
+    "HTTPTelegramClient",
+    "HTTPAssistantlient",
+]
