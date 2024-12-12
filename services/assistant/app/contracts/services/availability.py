d27f9af6ad548519d522501d1ec4fc828f5393e5
infanasotku <infanasotku@gmail.com>
2024-12-12 11:41:03 +0700

    Add new params to check availability/Add first implementation

--- services/assistant/app/contracts/services/availability.py
+++ services/assistant/app/contracts/services/availability.py
@@ -8,11 +8,19 @@ from app.schemas.availability import (
 
 class AvailabilityService(ABC):
     @abstractmethod
-    async def check_availability(self, service: Service) -> AvailabilitySchema:
+    async def check_availability(
+        self,
+        service: Service,
+        retries_count: int = 10,
+        notify_factor_level: float = 0.5,
+    ) -> AvailabilitySchema:
         """Checks availability of service.
 
         - Sends notify to telegram bot if service not available.
         - Logs service available status.
+
+        :param retries_count: count of availability checks for counts availability factor.
+        :param notify_factor_level: minimal level for sending notify to telegram bot.
 
         :return: Logged availability record.
         """
--- services/assistant/app/services/availability.py
+++ services/assistant/app/services/availability.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+from typing import Awaitable, Callable
+
 from app.contracts.repositories import AvailabilityRepository
 from app.contracts.services import AvailabilityService
-from app.schemas.availability import AvailabilitySchema, Service
 from app.contracts.clients import BookingClient, XrayClient, AssistantClient
+
+from app.schemas.availability import (
+    AvailabilitySchema,
+    Service,
+    AvailabilityCreateSchema,
+)
 
 
 class AvailabilityServiceImpl(AvailabilityService):
@@ -17,5 +25,47 @@ class AvailabilityServiceImpl(AvailabilityService):
         self._xray_client = xray_client
         self._assistant_client = assistant_client
 
-    async def check_availability(self, service: Service) -> AvailabilitySchema:
-        raise NotImplementedError
+    async def check_availability(
+        self,
+        service: Service,
+        retries_count: int = 10,
+        notify_factor_level: float = 0.5,
+    ) -> AvailabilitySchema:
+        average_response_time = 0
+        availability_count = 0
+
+        check_health: Callable[[], Awaitable[bool]]
+
+        match service:
+            case Service.xray:
+                check_health = self._xray_client.check_health
+            case Service.booking:
+                check_health = self._booking_client.check_health
+            case Service.assistant:
+                check_health = self._assistant_client.check_health
+            case _:
+                raise Exception(f"Undefined service: {service.name}.")
+
+        for _ in range(retries_count):
+            start = datetime.now()
+            healthy = await check_health()
+            end = datetime.now()
+
+            if healthy:
+                availability_count += 1
+                average_response_time += (end - start).microseconds * 1000
+
+        availability_factor = availability_count / retries_count
+        average_response_time /= availability_count
+
+        availability_create = AvailabilityCreateSchema(
+            created=datetime.now(),
+            service=service,
+            availability_factor=availability_factor,
+            response_time=average_response_time,
+        )
+
+        await self._availability_repository.log_availability(availability_create)
+
+        if availability_factor >= notify_factor_level:
+            pass  # TODO: send notify to bot
