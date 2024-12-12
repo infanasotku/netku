067b676082716317c9a37ebe9ecab1b288df9394
infanasotku <infanasotku@gmail.com>
2024-12-12 16:08:31 +0700

    Added base contract to base sql repo

--- services/assistant/app/infra/database/sql_db/repositories/base.py
+++ services/assistant/app/infra/database/sql_db/repositories/base.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.contracts.repositories.base import BaseRepository
 
-class SQLBaseRepository:
+
+class SQLBaseRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
         self._session = session
