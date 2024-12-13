d70c751a7b9c340c9211012620e8cff6a36ce9d1
infanasotku <infanasotku@gmail.com>
2024-12-13 14:06:19 +0700

    Groupped entry point for starting sheduled tasks

--- services/assistant/manage.py
+++ services/assistant/manage.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI
 import uvicorn
 
@@ -11,47 +12,65 @@ from app.adapters.bot import BotAppFactory, BotServicesFactory, BotSettings
 from app.tasks.restart_proxy import RestartProxyTask
 
 
-def create_app() -> FastAPI:
-    dependencies = AssistantDependencies(settings)
+def run_backend(dependencies: AssistantDependencies):
+    def create_app(dependencies: AssistantDependencies) -> FastAPI:
+        bot_factory = BotAppFactory(
+            bot=dependencies.bot,
+            bot_settings=BotSettings(**settings.model_dump()),
+            bot_services_factory=BotServicesFactory(
+                create_user_service=dependencies.create_user_service,
+                create_booking_service=dependencies.create_booking_service,
+                create_xray_service=dependencies.create_xray_service,
+                create_booking_analysis_service=dependencies.create_booking_analysis_service,
+            ),
+            logger=logger,
+        )
 
-    bot_factory = BotAppFactory(
-        bot=dependencies.bot,
-        bot_settings=BotSettings(**settings.model_dump()),
-        bot_services_factory=BotServicesFactory(
+        # Tasks
+        restart_proxy = RestartProxyTask(
+            bot=dependencies.bot,
             create_user_service=dependencies.create_user_service,
-            create_booking_service=dependencies.create_booking_service,
             create_xray_service=dependencies.create_xray_service,
-            create_booking_analysis_service=dependencies.create_booking_analysis_service,
-        ),
-        logger=logger,
-    )
+            logger=logger,
+            xray_restart_minutes=settings.xray_restart_minutes,
+        )
 
-    # Tasks
-    restart_proxy = RestartProxyTask(
-        bot=dependencies.bot,
-        create_user_service=dependencies.create_user_service,
-        create_xray_service=dependencies.create_xray_service,
-        logger=logger,
-        xray_restart_minutes=settings.xray_restart_minutes,
-    )
+        factory = AppFactory(logger)
+        factory.register_sub_factory(bot_factory)
 
-    factory = AppFactory(logger)
-    factory.register_sub_factory(bot_factory)
+        factory.register_task(restart_proxy)
 
-    factory.register_task(restart_proxy)
+        return factory.create_app()
 
-    return factory.create_app()
-
-
-def run():
     uvicorn.run(
-        app=create_app(),
+        app=create_app(dependencies),
         host=settings.host,
         port=settings.port,
         log_config=config,
         ssl_keyfile=settings.ssl_keyfile,
         ssl_certfile=settings.ssl_certfile,
     )
+
+
+def run_sheduled_tasks(dependencies: AssistantDependencies):
+    from app.schemas.availability import Service
+
+    async def start():
+        async with dependencies.create_availability_service() as avail_service:
+            await avail_service.check_availability(Service.xray)
+
+    asyncio.run(start())
+
+
+def run():
+    param = "shedule"
+
+    dependencies = AssistantDependencies(settings)
+
+    if param == "shedule":
+        run_sheduled_tasks(dependencies)
+    else:
+        run_backend(dependencies)
 
 
 if __name__ == "__main__":
