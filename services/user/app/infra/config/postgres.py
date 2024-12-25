a4398060f532647e9c06fe5aad99e197a003f336
infanasotku <infanasotku@gmail.com>
2024-12-25 12:28:45 +0700

    Add schema to conn

--- services/user/app/infra/config/postgres.py
+++ services/user/app/infra/config/postgres.py
@@ -5,6 +5,7 @@ from pydantic import Field, computed_field
 class PostgreSQLSettings(BaseSettings):
     psql_pass: str = Field(validation_alias="POSTGRES_PASSWORD")
     psql_user: str = Field(validation_alias="POSTGRES_USER")
+    psql_schema: str = Field(validation_alias="POSTGRES_SCHEMA")
     psql_host: str = Field(validation_alias="POSTGRES_HOST", default="127.0.0.1")
     psql_port: int = Field(validation_alias="POSTGRES_PORT", default=5432)
     psql_db_name: str = Field(validation_alias="POSTGRES_DB_NAME", default="postgres")
