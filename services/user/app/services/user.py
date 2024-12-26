7a66dbe459356f3e4c61ec27787a0915e063ed57
infanasotku <infanasotku@gmail.com>
2024-12-26 11:54:39 +0700

    Add user service implementation

---
+++ services/user/app/services/user.py
@@ -0,0 +1,24 @@
+from app.contracts.repositories import UserRepository
+from app.contracts.services import UserService
+
+from app.schemas.user import UserSchema, UserUpdateSchema
+
+
+class UserServiceImpl(UserService):
+    def __init__(self, user_repository: UserRepository):
+        self._user_repository = user_repository
+
+    async def get_users_by_id(self, ids):
+        return await self._user_repository.get_users_by_id(ids)
+
+    async def get_user_by_telegram_id(self, id: int) -> UserSchema | None:
+        return await self._user_repository.get_user_by_telegram_id(id)
+
+    async def get_user_by_phone(self, phone: str) -> UserSchema | None:
+        return await self._user_repository.get_user_by_phone(phone)
+
+    async def get_users(self) -> list[UserSchema]:
+        return await self._user_repository.get_all_users()
+
+    async def update_user(self, user_id, user_update: UserUpdateSchema) -> UserSchema:
+        return await self._user_repository.update_user(user_id, user_update)
