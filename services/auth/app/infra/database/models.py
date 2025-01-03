2f1873a8cbf8d009985279b6ea8050ce58ef9aff
infanasotku <infanasotku@gmail.com>
2025-01-04 03:05:04 +0700

    Edit models for views

--- services/auth/app/infra/database/models.py
+++ services/auth/app/infra/database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from common.sql.orm import Base
 
@@ -7,12 +7,18 @@ from common.sql.orm import Base
 class Client(Base):
     __tablename__ = "clients"
 
+    def __str__(self):
+        return f"[Client] {self.client_id}"
+
     client_id: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
     hashed_client_secret: Mapped[str] = mapped_column(nullable=False)
 
 
 class Scope(Base):
     __tablename__ = "scopes"
+
+    def __str__(self):
+        return f"[Scope] {self.name}"
 
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
@@ -23,6 +29,9 @@ class ClientScope(Base):
     client_id: Mapped[int] = mapped_column(
         ForeignKey("clients.id"), nullable=False, index=True
     )
+    client: Mapped["Client"] = relationship("Client")
+
     scope_id: Mapped[int] = mapped_column(
         ForeignKey("scopes.id"), nullable=False, index=True
     )
+    scope: Mapped["Scope"] = relationship("Scope")
