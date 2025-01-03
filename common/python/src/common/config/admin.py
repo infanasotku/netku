c44114e4e5f6b3c76bc3e1c53e0e1e3729c6e844
infanasotku <infanasotku@gmail.com>
2025-01-04 02:21:20 +0700

    Change env name

--- common/python/src/common/config/admin.py
+++ common/python/src/common/config/admin.py
@@ -3,5 +3,5 @@ from pydantic import Field
 
 
 class AdminSettings(BaseSettings):
-    username: str = Field(validation_alias="ADMIN_USERNAME")
-    password: int = Field(validation_alias="ADMIN_PASSWORD")
+    admin_username: str = Field(validation_alias="ADMIN_USERNAME")
+    admin_password: int = Field(validation_alias="ADMIN_PASSWORD")
