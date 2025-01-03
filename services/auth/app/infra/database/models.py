9abb740d56f62e328d7ca17e59676cbc48c318fc
infanasotku <infanasotku@gmail.com>
2025-01-03 21:48:55 +0700

    Add models

--- services/auth/app/infra/database/models.py
+++ services/auth/app/infra/database/models.py
@@ -1,1 +1,28 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
+
 from common.sql.orm import Base
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    client_id: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
+    hashed_client_secret: Mapped[str] = mapped_column(nullable=False)
+
+
+class Scopes(Base):
+    __tablename__ = "scopes"
+
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+
+class ClientScope(Base):
+    __tablename__ = "client_scopes"
+
+    client_id: Mapped[int] = mapped_column(
+        ForeignKey("clients.id"), nullable=False, index=True
+    )
+    scope_id: Mapped[int] = mapped_column(
+        ForeignKey("scopes.id"), nullable=False, index=True
+    )
