f6bc823cce124421d326abfe9105df45f9c9fa31
infanasotku <infanasotku@gmail.com>
2025-01-05 20:39:02 +0700

    Add security client implementation

---
+++ services/auth/app/infra/security.py
@@ -0,0 +1,35 @@
+from datetime import timedelta, datetime
+from passlib.context import CryptContext
+import jwt
+
+from app.contracts.clients import SecurityClient
+from app.schemas.client import TokenPayload
+
+
+class SecurityClientImpl(SecurityClient):
+    algorithm = "RS256"
+
+    def __init__(self, secret: str, *, expires_delta: timedelta | None = None):
+        self.context = CryptContext(schemes=["bcrypt"])
+        self.expires_delta = expires_delta or timedelta(minutes=15)
+        self.secret = secret
+
+    def get_hash(self, source: str) -> str:
+        return self.context.hash(source)
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.context.verify(plain_password, hashed_password)
+
+    def create_access_token(self, data: dict) -> str:
+        expire = datetime.now() + self.expires_delta
+        return jwt.encode(
+            {"exp": expire, **data}, self.secret, algorithm=self.algorithm
+        )
+
+    def parse_access_token(self, token: str) -> TokenPayload:
+        payload: dict = jwt.decode(token, self.secret, algorithms=self.algorithm)
+        client_id = payload.get("sub")
+        if client_id is None:
+            raise KeyError("Missing sub in the token.")
+        scopes = payload.get("scopes", [])
+        return TokenPayload(client_id=client_id, scopes=scopes)
