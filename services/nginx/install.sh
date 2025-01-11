859767de863740ce1f55d28d6cce58e16b0d8f7e
infanasotku <infanasotku@gmail.com>
2025-01-12 00:08:38 +0700

    Remove extra run/Correct args for new folder name

--- services/nginx/install.sh
+++ services/nginx/install.sh
@@ -1,5 +1,5 @@
 touch temp
-envsubst '${DOMAIN},${API_GATEWAY_ADDR},${XRAY_FALLBACK_PORT},${AUTH_ADDR}' \
+envsubst '${DOMAIN},${KONG_ADDR},${XRAY_FALLBACK_PORT},${AUTH_ADDR}' \
  < /etc/nginx/conf.d/nginx.conf \
  > temp
 cat temp > /etc/nginx/conf.d/nginx.conf
--- services/nginx/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Waits for resolving assistant dns
-while ! nslookup assistant; do
-    sleep 5
-done
-
-# Runs nginx
-nginx -g 'daemon off;'
