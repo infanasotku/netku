04bbafcfac50f217d91b7686b591aa766b75e7cf
infanasotku <infanasotku@gmail.com>
2025-01-12 14:09:40 +0700

    Correct nginx dockerfile

--- services/nginx/install.sh
+++ services/nginx/install.sh
@@ -1,6 +1,6 @@
 touch temp
 envsubst '${DOMAIN},${KONG_ADDR},${XRAY_FALLBACK_PORT},${AUTH_ADDR}' \
- < /etc/nginx/conf.d/nginx.conf \
+ < /etc/nginx/conf.d/default.conf \
  > temp
-cat temp > /etc/nginx/conf.d/nginx.conf
+cat temp > /etc/nginx/conf.d/default.conf
 rm temp
