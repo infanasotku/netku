d1211d25b379481024c5a9449f3d72280cc19b3c
infanasotku <infanasotku@gmail.com>
2025-01-31 00:35:11 +0700

    Add rabbit container

---
+++ common/python/src/common/containers/rabbitmq.py
@@ -0,0 +1,32 @@
+from typing import Any, AsyncGenerator
+from dependency_injector import containers, providers
+import aio_pika
+
+from common.containers.base import BaseContainer
+
+
+async def _get_connection(
+    *, username: str, password: str, host: str, port: int, virtualhost: str
+) -> AsyncGenerator[aio_pika.abc.AbstractRobustConnection, Any]:
+    connection = await aio_pika.connect_robust(
+        login=username,
+        password=password,
+        host=host,
+        port=port,
+        virtualhost=virtualhost,
+    )
+    yield connection
+
+
+@containers.copy(BaseContainer)
+class RabbitMQContainer(BaseContainer):
+    connection = providers.Resource(
+        _get_connection,
+        username=BaseContainer.config.rabbit_user,
+        password=BaseContainer.config.rabbit_pass,
+        host=BaseContainer.config.rabbit_host,
+        port=BaseContainer.config.rabbit_port,
+        virtualhost=BaseContainer.config.rabbit_vhost,
+    )  # Do not use with Closing marker!
+
+    channel = providers.Singleton(connection.provided.channel)
