346d9dd479fbd905f32cd769edac4fadfd3fafb5
infanasotku <infanasotku@gmail.com>
2025-01-31 00:35:46 +0700

    Add new methods to repo

--- services/auth/app/contracts/repositories/client_scope.py
+++ services/auth/app/contracts/repositories/client_scope.py
@@ -5,5 +5,19 @@ from common.contracts.repository import BaseRepository
 
 class ClientScopeRepository(BaseRepository):
     @abstractmethod
-    async def get_scopes_by_client_id(self, id: int) -> list[str]:
+    async def get_client_id_by_client_scope_id(self, client_scope_id: int) -> int:
+        pass
+
+    @abstractmethod
+    async def get_scopes_by_client_id(self, client_id: int) -> list[str]:
+        pass
+
+    @abstractmethod
+    async def remove_client_scope(self, client_scope_id: int):
+        pass
+        """Removes client scope.
+
+        Returns:
+            Relevant client scopes.
+        """
         pass
