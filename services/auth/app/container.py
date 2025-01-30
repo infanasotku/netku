3d056f6bdd236628518e779449769871648e66b2
infanasotku <infanasotku@gmail.com>
2025-01-31 00:58:46 +0700

    Add config and container for scope changing event

--- services/auth/app/container.py
+++ services/auth/app/container.py
@@ -1,5 +1,7 @@
 from dependency_injector import containers, providers
+
 from common.containers.postgres import PostgresContainer
+from common.containers.rabbitmq import RabbitMQContainer
 from common.containers.utils import with_context
 
 from app.adapters.output.database.repositories import (
@@ -8,11 +10,13 @@ from app.adapters.output.database.repositories import (
 )
 from common.auth import PyJWTSecurityClient
 from app.services.client import ClientServiceImpl
+from common.messaging.clients import RabbitMQOutClient
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
+    rabbit_container = providers.Container(RabbitMQContainer, config=config)
     postgres_container = providers.Container(PostgresContainer, config=config)
 
     client_repository = providers.Factory(
@@ -26,9 +30,15 @@ class Container(containers.DeclarativeContainer):
         PyJWTSecurityClient, config.public_key, config.private_key
     )
 
+    scope_message_client = providers.Factory(
+        RabbitMQOutClient,
+        rabbit_container.container.channel,
+        routing_key=config.scopes_routing_key,
+    )
     client_service = providers.Factory(
         with_context(ClientServiceImpl),
         security_client,
         client_scope_repository,
         client_repository,
+        scope_message_client,
     )
--- services/auth/app/infra/config/__init__.py
+++ services/auth/app/infra/config/__init__.py
@@ -1,10 +1,19 @@
+from pydantic import Field
+
 from common.config import (
     PostgreSQLSettings,
     NetworkSettings,
     AdminSettings,
     LocalAuthSettings,
+    RabbitMQSettings,
 )
 
 
-class Settings(PostgreSQLSettings, NetworkSettings, AdminSettings, LocalAuthSettings):
-    pass
+class Settings(
+    PostgreSQLSettings,
+    NetworkSettings,
+    AdminSettings,
+    LocalAuthSettings,
+    RabbitMQSettings,
+):
+    scopes_routing_key: str = Field(validation_alias="SCOPES_ROUTING_KEY")
