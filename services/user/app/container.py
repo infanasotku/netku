baf550c9f515d0f10053a0d35bd26d9aef2460a5
infanasotku <infanasotku@gmail.com>
2025-02-07 23:39:22 +0700

    Add message in client

--- services/user/app/container.py
+++ services/user/app/container.py
@@ -1,19 +1,39 @@
 from dependency_injector import providers, containers
 from common.containers.auth import LocalAuthContainer
 from common.containers.postgres import PostgresContainer
+from common.containers.rabbitmq import RabbitMQContainer, create_queue, get_exchange
 from common.containers.utils import with_context
 
 from app.adapters.output.database.repositories import (
     SQLUserRepository,
 )
 from app.services.user import UserServiceImpl
+from common.messaging.clients import RabbitMQInClient
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
+    rabbit_container = providers.Container(RabbitMQContainer, config=config)
     postgres_container = providers.Container(PostgresContainer, config=config)
     auth_container = providers.Container(LocalAuthContainer, config=config)
+
+    scope_exchange = providers.Singleton(
+        get_exchange,
+        rabbit_container.container.channel,
+        exchange_name=config.scope_exchange_name,
+    )
+    scope_queue = providers.Singleton(
+        create_queue,
+        rabbit_container.container.channel,
+        scope_exchange,
+        queue_name=config.scope_queue_name,
+        routing_key=config.scope_routing_key,
+    )
+    scope_message_client = providers.Singleton(
+        RabbitMQInClient,
+        scope_queue,
+    )
 
     user_repository = providers.Factory(
         with_context(SQLUserRepository), postgres_container.container.session
--- services/user/app/infra/config/__init__.py
+++ services/user/app/infra/config/__init__.py
@@ -1,10 +1,26 @@
+from pydantic import Field, computed_field
+import uuid
+
 from common.config import (
     PostgreSQLSettings,
     NetworkSettings,
     LocalAuthSettings,
     AdminSettings,
+    RabbitMQSettings,
 )
 
 
-class Settings(PostgreSQLSettings, NetworkSettings, LocalAuthSettings, AdminSettings):
-    pass
+class Settings(
+    PostgreSQLSettings,
+    NetworkSettings,
+    LocalAuthSettings,
+    AdminSettings,
+    RabbitMQSettings,
+):
+    scope_exchange_name: str = Field(validation_alias="SCOPE_EXCHANGE_NAME")
+    scope_routing_key: str = Field(validation_alias="SCOPE_ROUTING_KEY")
+
+    @computed_field
+    @property
+    def scope_queue_name(self) -> str:
+        return f"user_{uuid.uuid4()}"
