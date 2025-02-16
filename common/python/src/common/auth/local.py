a43c69aa640f7baf551b6828a1a29d0f1bcb2dde
infanasotku <infanasotku@gmail.com>
2025-02-16 13:36:37 +0700

    Completed handling token update

--- common/python/src/common/auth/local.py
+++ common/python/src/common/auth/local.py
@@ -7,9 +7,16 @@ class LocalAuthService(AuthService):
     def __init__(self, security_client: SecurityClient):
         self.security_client = security_client
 
+        self._cached_credentials: dict[str, ClientCredentials] = {}
+
     async def process_update(self, credentials: ClientCredentials):
         """Processes scopes updates."""
-        print("Credentials", credentials)
+        self._cached_credentials[credentials.external_client_id] = credentials
 
     async def introspect(self, token):
-        return self.security_client.parse_access_token(token)
+        payload = self.security_client.parse_access_token(token)
+
+        if payload.external_client_id in self._cached_credentials:
+            payload.scopes = self._cached_credentials[payload.external_client_id].scopes
+
+        return payload
