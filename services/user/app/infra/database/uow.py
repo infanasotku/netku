08cfd836eb77dbd342b50d962a719a5f83f57bb3
infanasotku <infanasotku@gmail.com>
2025-02-16 22:52:34 +0700

    Removed extra annotation

--- services/user/app/infra/database/uow.py
+++ services/user/app/infra/database/uow.py
@@ -1,10 +1,8 @@
 from typing import Self
 
 from common.sql.uow import SQLUnitOfWork
+
 from app.contracts.uow import UserUnitOfWork
-
-from app.contracts.repositories import UserRepository
-
 from app.infra.database.repositories import (
     SQLUserRepository,
 )
@@ -13,5 +11,5 @@ from app.infra.database.repositories import (
 class SQLUserUnitOfWork(SQLUnitOfWork, UserUnitOfWork):
     async def __aenter__(self) -> Self:
         uow = await super().__aenter__()
-        self.user: UserRepository = SQLUserRepository(self._session)
+        self.user = SQLUserRepository(self._session)
         return uow
