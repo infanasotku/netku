e44c71e52b847fa0b63fef5d616b4ca6b6990e18
infanasotku <infanasotku@gmail.com>
2025-02-17 23:41:27 +0700

    Add contracts for proxy service

---
+++ services/proxy/app/contracts/services/proxy.py
@@ -0,0 +1,51 @@
+from abc import abstractmethod
+
+from common.contracts.services import BaseService
+
+from app.schemas.proxy import (
+    ProxyInfoCreateSchema,
+    ProxyInfoSchema,
+    ProxyInfoUpdateSchema,
+)
+
+
+class ProxyService(BaseService):
+    async def get_proxy_info(self) -> ProxyInfoSchema | None:
+        """
+        Returns:
+            Proxy info if it exist in db, `None` otherwise.
+        """
+
+    @abstractmethod
+    async def create_proxy_info(
+        self, proxy_create: ProxyInfoCreateSchema
+    ) -> ProxyInfoSchema:
+        """Creates proxy info if it not exist in db.
+
+        Raises:
+            ValueError: If info already exist.
+        Returns:
+            Created info if it created success.
+        """
+
+    @abstractmethod
+    async def update_proxy_info(
+        self, proxy_update: ProxyInfoUpdateSchema
+    ) -> ProxyInfoSchema:
+        """Updates proxy info if it exist in db.
+
+        Raises:
+            ValueError: If info not exist.
+        Returns:
+            Updated info if it updated success.
+        """
+
+    @abstractmethod
+    async def sync_with_xray(self) -> ProxyInfoSchema:
+        """Syncs saved proxy info with xray by restarting it.
+
+        Sends corresponding event about info changing.
+
+        Returns:
+            Synced proxy info.
+        """
