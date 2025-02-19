70601cdb45f00728e3e2a2b7d6f09f689c335df5
infanasotku <infanasotku@gmail.com>
2025-02-19 17:02:14 +0700

    Added remote client contract

--- common/python/src/common/contracts/clients/__init__.py
+++ common/python/src/common/contracts/clients/__init__.py
@@ -1,5 +1,12 @@
 from common.contracts.clients.security import SecurityClient
 from common.contracts.clients.base import BaseClient
+from common.contracts.clients.remote import RemoteBaseClient
 from common.contracts.clients.message import MessageInClient, MessageOutClient
 
-__all__ = ["SecurityClient", "BaseClient", "MessageInClient", "MessageOutClient"]
+__all__ = [
+    "SecurityClient",
+    "BaseClient",
+    "MessageInClient",
+    "MessageOutClient",
+    "RemoteBaseClient",
+]
---
+++ common/python/src/common/contracts/clients/remote.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod
+
+from common.contracts.clients.base import BaseClient
+
+
+class RemoteBaseClient(BaseClient):
+    @abstractmethod
+    async def check_health(self) -> bool:
+        """Checks of remote service.
+
+        :return: `True` if service available, `False` otherwise."""
