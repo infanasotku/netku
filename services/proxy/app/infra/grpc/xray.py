225bb214b76b2e5cd955d11633301907998a4e22
infanasotku <infanasotku@gmail.com>
2025-02-20 10:19:56 +0700

    Added xray client grpc implementation

---
+++ services/proxy/app/infra/grpc/xray.py
@@ -0,0 +1,23 @@
+from uuid import UUID, uuid4
+
+from common.grpc import BaseGRPCClient
+
+from app.infra.grpc.gen.xray_pb2_grpc import XrayStub
+from app.infra.grpc.gen.xray_pb2 import RestartResponse, RestartRequest
+from app.contracts.clients import XrayClient
+
+
+class GRPCXrayClient(BaseGRPCClient, XrayClient):
+    service_name = "xray"
+
+    async def restart(self, uuid: UUID | None = None) -> UUID | None:
+        if uuid is None:
+            uuid = uuid4()
+
+        healthy = await self.check_health()
+        if not healthy:
+            return
+
+        stub = XrayStub(self._channel)
+        resp: RestartResponse = await stub.RestartXray(RestartRequest(uuid=str(uuid)))
+        return UUID(resp.uuid)
