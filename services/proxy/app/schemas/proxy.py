8088fcb9d7751d0a62912c5ed4887c6c487ebad1
infanasotku <infanasotku@gmail.com>
2025-02-27 10:44:10 +0700

    Added create and update proxy schema

---
+++ services/proxy/app/schemas/proxy.py
@@ -0,0 +1,16 @@
+import datetime
+from uuid import UUID
+from pydantic import Field
+from common.schemas import BaseSchema
+
+
+class ProxyInfoCreateSchema(BaseSchema):
+    running: bool = Field(default=False)
+    created: datetime
+    uuid: UUID
+    addr: str
+
+
+class ProxyInfoUpdateSchema(BaseSchema):
+    running: bool | None = None
+    uuid: UUID | None = None
