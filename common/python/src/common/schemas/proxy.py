f1a5775c294507636ed613d73f5d31539675132b
infanasotku <infanasotku@gmail.com>
2025-02-27 10:43:50 +0700

    Corrected common proxy schema

--- common/python/src/common/schemas/proxy.py
+++ common/python/src/common/schemas/proxy.py
@@ -1,7 +1,13 @@
+from datetime import datetime
 from uuid import UUID
+
+from pydantic import Field
 
 from common.schemas import BaseSchema
 
 
-class ProxyInfo(BaseSchema):
+class ProxyInfoSchema(BaseSchema):
+    running: bool = Field(default=False)
+    created: datetime
     uuid: UUID
+    addr: str
