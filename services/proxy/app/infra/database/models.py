2b31403301a0b0fbc9b2144445e1cb6474ac35a2
infanasotku <infanasotku@gmail.com>
2025-02-27 10:56:13 +0700

    Corrected model

---
+++ services/proxy/app/infra/database/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from uuid import UUID
+from sqlalchemy.orm import mapped_column, Mapped
+
+from common.sql.orm import Base
+
+
+class ProxyInfo(Base):
+    __tablename__ = "proxy_info"
+
+    uuid: Mapped[UUID] = mapped_column(nullable=True)
+    created: Mapped[datetime] = mapped_column(nullable=False)
+    addr: Mapped[str] = mapped_column(nullable=False)
+    running: Mapped[bool] = mapped_column(nullable=False, default=False)
