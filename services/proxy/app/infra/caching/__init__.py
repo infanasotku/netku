8ee524e33f086a12c797e39dabf3c23bbf898cb4
infanasotku <infanasotku@gmail.com>
2025-03-02 12:48:32 +0700

    Added filter for engine keys

--- services/proxy/app/infra/caching/__init__.py
+++ services/proxy/app/infra/caching/__init__.py
@@ -1,3 +1,4 @@
 from app.infra.caching.redis import RedisProxyCachingClient
+from app.infra.caching.filter import create_egnine_keys_filter
 
-__all__ = ["RedisProxyCachingClient"]
+__all__ = ["RedisProxyCachingClient", "create_egnine_keys_filter"]
