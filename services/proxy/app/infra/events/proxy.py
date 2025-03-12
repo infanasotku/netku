825e4e271aa5694a1ce207b6acad2f3078bef092
infanasotku <infanasotku@gmail.com>
2025-03-12 14:43:42 +0700

    Added low level events

---
+++ services/proxy/app/infra/events/proxy.py
@@ -0,0 +1,34 @@
+from common.schemas import BaseSchema
+from common.events.proxy import ProxyTerminatedEvent, BaseEvent
+
+
+class KeyEventSchema(BaseSchema):
+    key: str
+
+
+class KeyHSetEvent(BaseEvent[KeyEventSchema]):
+    name = "keyevent.hset"
+
+    @staticmethod
+    def _dumps(data):
+        return data.model_dump_json()
+
+    @staticmethod
+    def _loads(payload):
+        if isinstance(payload, str):
+            return KeyEventSchema.model_validate_json(payload)
+        return KeyEventSchema.model_validate(payload)
+
+
+class KeyExpiredEvent(ProxyTerminatedEvent):
+    name = "keyevent.expired"
+
+    @staticmethod
+    def _dumps(data):
+        return data.model_dump_json()
+
+    @staticmethod
+    def _loads(payload):
+        if isinstance(payload, str):
+            return KeyEventSchema.model_validate_json(payload)
+        return KeyEventSchema.model_validate(payload)
